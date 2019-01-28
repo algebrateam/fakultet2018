a1a489bb5d8377f89ecbcb50b68692107b2b4b9b
mrvic <mrvic.predrag@gmail.com>
2019-01-28 20:41:52 +0100

    FIX#4 ispravljen adresa seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,10 +12,10 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(UsersTableSeeder::class);
+        $this->call(AdresasTableSeeder::class);
         $this->call(DrzavasTableSeeder::class);
         $this->call(TrgovinaTableSeeder::class);
         $this->call(MobitelTableSeeder::class);
-        $this->call(MobitelTrgovinaPivotSeeder::class);       
-        $this->call(AdresasTableSeeder::class);
+        $this->call(MobitelTrgovinaPivotSeeder::class);           
     }
 }
